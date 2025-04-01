93339df1d068316a387890a8c96fe271f177d192
carolurbs <carolineurb.dev@outlook.com>
2025-03-31 21:45:30 -0300

    form entry

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-
+item : string = ''
+addItem(){
+  console.log("Item recebido: ", this.item)
+  this.item="";
+}
 }
